acf08f63dbe8ed66dc42f3134ebc6420b2905065
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2017-10-02 14:11:43 -0300

    Terminando Aula 6, de Comparação de Variáveis

---
+++ Project/Variables/Variables3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        /* Comparação com if
+            int myNum = 10;
+            int myNumTwo = 20;
+            
+            if (myNum == myNumTwo)
+            {
+                Console.WriteLine("Numbers are equal!");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Numbers are inequal!");
+                Console.ReadKey();
+            }
+            */
+
+        // Comparação com switch
+        int myNum = 10;
+
+        switch (myNum)
+        {
+            case 1:
+                Console.WriteLine("Num is 1");
+                Console.ReadKey();
+                break;
+            case 2:
+                Console.WriteLine("Num is 2");
+                Console.ReadKey();
+                break;
+            case 3:
+                Console.WriteLine("Num is 3");
+                Console.ReadKey();
+                break;
+            default:
+                Console.WriteLine("Default case");
+                Console.ReadKey();
+                break;
+        }
+    }
+}
