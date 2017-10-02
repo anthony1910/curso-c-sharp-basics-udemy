8701be9d1e52e1b4c5ea8aee7ee712b4e1dc86a5
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2017-10-02 14:38:13 -0300

    Terminando Aula 8, de Arrays

---
+++ Project/Arrays/Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* array de strings
+            string[] myArray = new string[] { "Learn", "app", "development" };
+
+            string word = myArray[1];
+
+            Console.WriteLine(word);
+            Console.ReadKey();
+            */
+
+            int[] intArray = new int[] { 232, 373, 32651 };
+
+            int arrayLength = intArray.Length;
+
+            Console.WriteLine(arrayLength.ToString()); // Se esforçar em colocar ToString() quando for falar de strings
+            Console.ReadKey();
+        }
+    }
+}
