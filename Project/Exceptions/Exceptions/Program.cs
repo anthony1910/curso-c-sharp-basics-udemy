0edb2a9513e0bc3fc51f7c7b26ff26697d266833
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2017-10-02 15:26:15 -0300

    Terminando Aula 12, de Exceções

---
+++ Project/Exceptions/Exceptions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Anthony";
+
+            try
+            {
+                int myNum = Convert.ToInt16(myName); // Erro de conversão
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
