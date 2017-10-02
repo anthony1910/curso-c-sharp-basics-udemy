105eb285c514458307c53aa0a30c3fe77fc5ee18
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2017-10-02 13:24:54 -0300

    Terminando Aula de Operadores

---
+++ Project/Operators/Operators/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int myNum = 9;
+            int otherNum = 12;
+            int result = myNum / otherNum;
+            */
+
+            /*
+            double myNum = 9;
+            double otherNum = 12;
+            double result = myNum / otherNum;
+            */
+
+            string name = "Anthony";
+            string lastName = "Ribeiro";
+
+            string fullName = name + " " + lastName;
+
+            Console.WriteLine(fullName);
+            Console.ReadKey();
+        }
+    }
+}
