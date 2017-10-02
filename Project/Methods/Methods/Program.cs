64cfb5cceaaa3bf0d4511c7b4a3f2307566673a1
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2017-10-02 14:48:15 -0300

    Terminando Aula 9. de Métodos

---
+++ Project/Methods/Methods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program test = new Program();
+            test.addNumbers(9, 10);
+        }
+        public void addNumbers(int myNum, int otherNum)
+        {
+            int result = myNum + otherNum;
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
