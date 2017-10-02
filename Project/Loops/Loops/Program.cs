786421130e88ae655f1bcec45f3c4e10dbe9f5de
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2017-10-02 14:21:36 -0300

    Terminando Aula 7, de Loops

---
+++ Project/Loops/Loops/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* while loop
+            int i = 0;
+
+            while (i < 100)
+            {
+                Console.WriteLine("Value of i = " + i.ToString());                
+                i++;
+            }
+            Console.ReadKey();
+            */
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("i = " + i.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
